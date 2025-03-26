d7f805a9423670c37c28c07e1aeb956997dfb56d
rabitis99 <kyungsu9914@gmail.com>
2025-03-26 11:21:22 +0900

    ãfeat lv5,lv6동시처리

---
+++ src/main/java/com/example/calendarproject/exception/ErrorCode.java
@@ -0,0 +1,4 @@
+package com.example.calendarproject.exception;
+
+public class ErrorCode {
+}
--- src/main/java/com/example/calendarproject/exception/ErrorDto.java
+++ src/main/java/com/example/calendarproject/exception/ErrorDto.java
@@ -1,4 +1,4 @@
 package com.example.calendarproject.dto;
 
-public class PageRequestDto {
+public class ErrorDto {
 }
